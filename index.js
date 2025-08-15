e8ad16ed0cb7957ac72465c9212d8ae4e836d1a8
Sudip Bhomjan <bhomjansudip18@gmail.com>
2025-08-15 08:54:09 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//import { z } from "zod";
+const z = require("zod");
+
+const addNumbersSchema = z.object({
+  a: z.number().min(1).max(100),
+  b: z.number().min(1).max(100),
+});
+
+function addNumbers(a, b) {
+  const result = addNumbersSchema.safeParse({ a, b });
+
+  if (!result.success) {
+    throw new Error(
+      `Invalid input: ${result.error.issues.map((e) => e.message).join(", ")}`
+    );
+  }
+
+  return a + b;
+}
+
+try {
+  //console.log(addNumbers(1, 2));
+   console.log(addNumbers("1", 2));
+  // console.log(addNumbers(150, 2));
+} catch (error) {
+  console.log(error.message);
+}
